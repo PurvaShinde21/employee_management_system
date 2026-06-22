236c20b79961221cb2e8a4bb050062921e9872bb
PurvaShinde21 <purvapshinde.21@gmail.com>
2026-06-22 12:20:36 +0000

    Fix ASG refresh command and add gitignore

--- terraform/modules/ec2_asg/variables.tf
+++ terraform/modules/ec2_asg/variables.tf
@@ -1,5 +1,20 @@
-variable "environment" { type = string }
-variable "instance_type" { type = string; default = "t3.micro" }
-variable "app_security_group_id" { type = string }
-variable "private_subnet_ids" { type = list(string) }
-variable "target_group_arns" { type = list(string) }
+variable "environment" {
+  type = string
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "app_security_group_id" {
+  type = string
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+}
+
+variable "target_group_arns" {
+  type = list(string)
+}
--- terraform/modules/rds/variables.tf
+++ terraform/modules/rds/variables.tf
@@ -1,7 +1,31 @@
-variable "environment" { type = string }
-variable "subnet_ids" { type = list(string) }
-variable "db_security_group_id" { type = string }
-variable "db_instance_class" { type = string; default = "db.t3.micro" }
-variable "db_username" { type = string; sensitive = true }
-variable "db_password" { type = string; sensitive = true }
-variable "multi_az" { type = bool; default = false }
+variable "environment" {
+  type = string
+}
+
+variable "subnet_ids" {
+  type = list(string)
+}
+
+variable "db_security_group_id" {
+  type = string
+}
+
+variable "db_instance_class" {
+  type    = string
+  default = "db.t3.micro"
+}
+
+variable "db_username" {
+  type      = string
+  sensitive = true
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "multi_az" {
+  type    = bool
+  default = false
+}
